f85259a82473976d075c64e33b51f60478ff476d
Katsuya Kondo <kondo.katsuya.0901@gmail.com>
2026-09-25 13:09:05 +0000

    notifications

--- docs/conf.py
+++ docs/conf.py
@@ -42,9 +42,9 @@ language = 'jp'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-html_static_path = ['_static']
-html_math_renderer="mathjax"
-
 html_theme_options = {
     "footer_items": ["copyright", "sphinx-version", "notifications"],
 }
+html_static_path = ['_static']
+html_math_renderer="mathjax"
+
